7d3d64e03fc6b97ad04c35114df794ec735a3ac9
八木健太 <yagi@monosense.co.jp>
2019-04-25 19:46:17 +0900

    リスト2-14

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -4,50 +4,24 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-global $head, $style, $body, $end;
-$head = '<html><head>';
-$style = <<<EOF
+class HelloController extends Controller
+{
+    public function __invoke()
+    {
+        return <<<EOF
+<html>
+<head>
+<title>Hello</title>
 <style>
 body { font-size:16pt; color:#999; }
 h1 { font-size: 100pt; text-align:right; color:#eee; margin:-40px 0px -50px 0px; }
 </style>
+</head>
+<body>
+    <h1>Single Action</h1>
+    <p>これは、シングルアクションコントローラのアクションです。</p>
+</body>
+</html>
 EOF;
-$body = '</head><body>';
-$end = '</body></head>';
-
-function tag($tag, $txt)
-{
-    return "<{$tag}>" . $txt . "</{$tag}>";
-}
-
-class HelloController extends Controller
-{
-    public function index()
-    {
-        global $head, $style, $body, $end;
-
-        $html = $head
-            . tag('title', 'Hello/Index')
-            . $style
-            . $body
-            . tag('h1', 'Index')
-            . tag('p', 'this is Index page')
-            . '<a href="/hello/other">go to Other page</a>'
-            . $end;
-        return $html;
-    }
-
-    public function other()
-    {
-        global $head, $style, $body, $end;
-
-        $html = $head
-            . tag('title', 'Hello/Other')
-            . $style
-            . $body
-            . tag('h1', 'Other')
-            . tag('p', 'this is Other page')
-            . $end;
-        return $html;
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -16,5 +16,4 @@ Route::get('/', function () {
 });
 
 
-Route::get('hello', 'HelloController@index');
-Route::get('hello/other', 'HelloController@other');
+Route::get('hello', 'HelloController');
