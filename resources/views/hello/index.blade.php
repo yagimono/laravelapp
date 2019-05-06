cebac1fd70a357caf01bdb2c3dfeedff42470a6c
八木健太 <yagi@monosense.co.jp>
2019-05-06 23:42:29 +0900

    りすと3-24

--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -2,22 +2,31 @@
 <head>
     <title>Hello/Index</title>
     <style>
-        body {font-size:16pt; color:#999;}
-        h1 {font-size:50pt; text-align:right; color:#f6f6f6; margin:-20px 0 -30px 0; letter-spacing:-4pt;}
+        body {
+            font-size: 16pt;
+            color: #999;
+        }
+
+        h1 {
+            font-size: 50pt;
+            text-align: right;
+            color: #f6f6f6;
+            margin: -20px 0 -30px 0;
+            letter-spacing: -4pt;
+        }
     </style>
 </head>
 <body>
 <h1>&#064;forでぃれくてぃぶの例</h1>
-<ol>
-    @for ($i = 1;$i < 100;$i++)
-        @if ($i % 2 == 1)
-            @continue
-        @elseif ($i <= 10)
-            <li>No, {{$i}}
-        @else
-            @break
-        @endif
-    @endfor
-</ol>
+@foreach ($data as $item)
+    @if ($loop->first)
+        <p>※データ一覧</p>
+        <ul>
+            @endif
+            <li>No,{{$loop->iteration}} . {{$item}}</li>
+            @if ($loop->last)
+        </ul><p>----ここまで</p>
+    @endif
+@endforeach
 </body>
 </html>
