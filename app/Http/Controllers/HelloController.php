70fcd0cc9c1a746104880bfd7d61be6e67b15b6a
八木健太 <yagi@monosense.co.jp>
2019-05-08 00:34:01 +0900

    りすと3-40

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -9,11 +9,6 @@ class HelloController extends Controller
 {
     public function index()
     {
-        $data = [
-            ['name'=>'山田太郎', 'mail'=>'taro@yamada'],
-            ['name'=>'田中花子', 'mail'=>'hanako@tanaka'],
-            ['name'=>'鈴木早智子', 'mail'=>'sachiko@ysuzuki'],
-        ];
-        return view('hello.index', ['data' => $data]);
+        return view('hello.index', ['message'=>'Hello!']);
     }
 }
--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -11,7 +11,8 @@
     <p>ここが本文のコンテンツです。</p>
     <p>必要なだけ記述できます。</p>
 
-    @each('components.item', $data, 'item')
+    <p>Controller value<br>'message' = {{$message}}</p>
+    <p>ViewComposer value<br>'view_message' = {{$view_message}}</p>
 @endsection
 
 @section('footer')
