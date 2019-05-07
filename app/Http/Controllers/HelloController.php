50126751f4c024eaa16c3617fbad70b0e8e0d82a
八木健太 <yagi@monosense.co.jp>
2019-05-08 00:07:20 +0900

    リスト3-31

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -9,7 +9,11 @@ class HelloController extends Controller
 {
     public function index()
     {
-        $data = ['one', 'twe', 'three', 'four', 'five'];
+        $data = [
+            ['name'=>'山田太郎', 'mail'=>'taro@yamada'],
+            ['name'=>'田中花子', 'mail'=>'hanako@tanaka'],
+            ['name'=>'鈴木早智子', 'mail'=>'sachiko@ysuzuki'],
+        ];
         return view('hello.index', ['data' => $data]);
     }
 }
