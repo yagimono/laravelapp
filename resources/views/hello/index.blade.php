a35731f30f73c1f806eab55659dc71d03d4aa73e
八木健太 <yagi@monosense.co.jp>
2019-05-07 21:42:37 +0900

    リスト3-27

--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -1,33 +1,17 @@
-<html>
-<head>
-    <title>Hello/Index</title>
-    <style>
-        body {
-            font-size: 16pt;
-            color: #999;
-        }
+@extends('layouts.helloapp')
 
-        h1 {
-            font-size: 50pt;
-            text-align: right;
-            color: #f6f6f6;
-            margin: -20px 0 -30px 0;
-            letter-spacing: -4pt;
-        }
-    </style>
-</head>
-<body>
-<h1>&#064;whileでぃれくてぃぶの例</h1>
-<ol>
-    @php
-    $counter = 0;
-    @endphp
-    @while ($counter < count($data))
-    <li>{{$data[$counter]}}</li>
-    @php
-    $counter++;
-    @endphp
-    @endwhile
-</ol>
-</body>
-</html>
+@section('title', 'Index')
+
+@section('menubar')
+    @parent
+    インデックスページ
+@endsection
+
+@section('content')
+    <p>ここが本文のコンテンツです。</p>
+    <p>必要なだけ記述できます。</p>
+@endsection
+
+@section('footer')
+copyright 2017 tuyano.
+@endsection
