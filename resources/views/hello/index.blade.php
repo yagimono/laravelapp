8c6cbbbe2ecdaf9daf7b3486d8851ce480cf25cd
八木健太 <yagi@monosense.co.jp>
2019-05-09 23:25:19 +0900

    リスト4-5

--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -10,9 +10,11 @@
 @section('content')
     <p>ここが本文のコンテンツです。</p>
     <p>必要なだけ記述できます。</p>
-
-    <p>Controller value<br>'message' = {{$message}}</p>
-    <p>ViewComposer value<br>'view_message' = {{$view_message}}</p>
+    <table>
+    @foreach($data as $item)
+        <tr><th>{{$item['name']}}</th><td>{{$item['mail']}}</td></tr>
+    @endforeach
+    </table>
 @endsection
 
 @section('footer')
